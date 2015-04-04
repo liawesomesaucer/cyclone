d73457a7085e76361a26df995038b5af2a6b613c
Vincent <vincent.oddrepublic@gmail.com>
2015-04-04 10:10:48 -0700

    fdsajkfls;jakl

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 DB_URL = 'postgres://svzgijjedzoxgo:lqiOequvQPlltINbMVmdLKKf1e@ec2-54-163-235-165.compute-1.amazonaws.com:5432/dct7rhlbci5ha1'
