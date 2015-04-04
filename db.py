784cb15409c8be5abdd77b3d61820e6f5b384703
Vincent <vincent.oddrepublic@gmail.com>
2015-04-04 10:23:09 -0700

    fdas

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 DB_URL = 'postgres://svzgijjedzoxgo:lqiOequvQPlltINbMVmdLKKf1e@ec2-54-163-235-165.compute-1.amazonaws.com:5432/dct7rhlbci5ha1'
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, echo=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
